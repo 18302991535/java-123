449b3d1319aa25e934e23c07a32d270d9c1bf0b9
wangxing <1115963060@qq.com>
2021-01-07 18:54:27 +0800

    这是一个副本

---
+++ User - 副本.java
@@ -0,0 +1,29 @@
+package cn.wx.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+private String id;
+    @Column(name="username",nullable = false,length = 20)
+private String username;
+    @Column(name="password",nullable = false,length = 50)
+private String password;
+    @Column(name="roleId",nullable = false,length = 50)
+private int roleId;
+    @Column(name="iconURL",nullable = false,length = 50)
+private String iconURL;
+    @Column(name="address",nullable=false,length=32)
+    private String address;
+}
